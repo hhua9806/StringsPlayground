d3257ff30f2f19ed8c9aca9676a87f3200194786
Hunter Huang <hunter@rexandjulie.com>
2016-09-15 09:37:07 -0600

    Footprints in the sand

---
+++ Contents.swift
@@ -0,0 +1,28 @@
+let firstString = "One night a man had a dream."
+var variableString = "He dreamed that he was walking along the beach with Sasquatch."
+let string1 = "Across the sky flashed scenes from his life."
+let string2 = "For each scene he noticed two set of footprints in the sand: one belonging to him, and the other to Sasquatch."
+let string3 = "When the last scene of his life flashed before him, he looked back at the footprints in the sand."
+let string4 = "He noticed that many times along the path of his life there was only one set of footprints."
+let string5 = "He also noticed that it happened at the very lowest and saddest times in his life."
+let string6 = "This really bothered the man, and he asked sasquatch about it."
+let string7 = "'Sasquatch, you said that once I decided to follow you, you'd walk with me all the way.'"
+let string8 = "'But I have noticed that during the most troublesome times in my life, there is only one set of footprints.'"
+let string9 = "'I don't understand why when I needed you most, you would leave me'"
+let string10 = "Sasquatch turned to the man and replied..."
+let string11 = "HUUUUUNNNNNHHH AAARRRGH!!!"
+
+let multiplier = 5
+let message = "\(multiplier) times 5 is\(Double(multiplier)* 5)"
+// message is "5 times 5 is 25"
+
+var word = "pneumonoultramicroscopicsilicovolcanoconiosis"
+print("The number of characters in \(word) is \(word.characters.count)"
+//Prints "The number of chracters in pneumonoultramicroscopicsilicovolcanoconiosis is 45"
+
+let quotation = "Recursion"
+let sameQuotation = "Recursion"
+if quotaion == sameQuotation {
+    print("These two strings are considered equal")
+}
+//Prints "These two string are considered equal"
